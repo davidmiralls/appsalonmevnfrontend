b87d937ee80203532efead3566743ee851d79e83
David Miralles <davidmiralls@gmail.com>
2025-06-26 17:05:06 +0200

    first commit

---
+++ src/views/appointments/AppointmentsLayout.vue
@@ -0,0 +1,39 @@
+import { RouterView } from 'vue-router';
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex justify-between">
+            <h1 class="text-2xl lg:text-6xl font-black text-white">AppSalón</h1>
+            <div class="flex flex-col space-y-5">
+                <div class="flex gap-2 items-center">
+                    <p class="text-white text-right">Hola: Usuario</p>
+                    <button
+                    class="bg-res-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg">
+                        Cerrar Sesión
+                    </button>
+
+                </div>
+                <nav class="flex gap-2 items-center justify-end">
+                    <button class="p-3 text-gray-200 uppercase text-xs font-black borunde-lg">Mis Citas</button>
+                    <RouterLink
+                    class="p-3 text-gray-200 uppercase text-xs font-black borunde-lg bg-blue-700"
+                    :to="{name: 'new-appointment'}"
+                    >
+                    Nueva Cita
+                </RouterLink>
+
+                </nav>
+
+            </div>
+    </div>
+    <main>
+        <RouterView />
+
+    </main>
+
+</template>
+    
+
+
---
+++ src/views/appointments/NewAppointmentLayout.vue
@@ -0,0 +1,34 @@
+import { RouterLink } from 'vue-router';
+        <script setup>
+        import { useRoute } from 'vue-router';
+
+        const route= useRoute()
+        
+        </script>
+<template>
+<nav class="my-5 flex gap-3">
+    <RouterLink
+    :to="{name:'new-appointment'}"
+    class="flex-1 text-center p3 uppercase font-extrabols hover:bg-blue-600 hover:text-white
+    "
+    :class="route.name === 'new-appointment' ? 'bg-blue-500 text-white':  'bg-white text-blue-500'"
+    >
+        Servicios
+    </RouterLink>
+    <RouterLink
+    :to="{name:'appointment-details'}"
+    class="flex-1 text-center p3 uppercase font-extrabols hover:bg-blue-600 hover:text-white
+    "
+     :class="route.name === 'appointment-details' ? 'bg-blue-500 text-white':  'bg-white text-blue-500'"
+    >
+        Cita y Resumen
+    </RouterLink>
+
+</nav>
+
+
+ 
+    <RouterView />
+</template>
+
+
